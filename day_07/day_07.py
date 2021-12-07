1037f1f48145e1b40f2d202e62f1bf24f9414f3a
HansBambel <Kevin.Trebing@gmx.net>
2021-12-07 08:25:25 +0100

    Solution day 7

---
+++ day_07/day_07.py
@@ -0,0 +1,39 @@
+from pathlib import Path
+
+import numpy as np
+from tqdm import tqdm
+
+
+def get_fuel_count_for_position(crab_submarines, part_two=False):
+    fuel_costs = np.zeros((np.max(crab_submarines)))
+    fuel_per_change = range(np.max(crab_submarines))
+    for pos in tqdm(range(np.max(crab_submarines))):
+        if not part_two:
+            fuel_costs[pos] = np.sum(abs(crab_submarines - pos))
+        else:
+            fuel_costs[pos] = np.sum([np.sum(fuel_per_change[: change + 1]) for change in abs(crab_submarines - pos)])
+    return fuel_costs
+
+
+crabs = np.array([16, 1, 2, 0, 4, 2, 7, 1, 2, 14], dtype=int)
+fuel_costs = get_fuel_count_for_position(crabs)
+
+assert fuel_costs[2] == 37
+assert fuel_costs[1] == 41
+assert fuel_costs[3] == 39
+assert fuel_costs[10] == 71
+print("Lowest position: ", np.argmin(fuel_costs))
+
+fuel_costs_new = get_fuel_count_for_position(crabs, part_two=True)
+assert fuel_costs_new[5] == 168
+assert fuel_costs_new[2] == 206
+
+with open(Path(__file__).parent / "input.txt", "r") as f:
+    crabs = f.readline()
+    crabs = crabs.split(",")
+crabs = np.array(crabs, dtype=int)
+fuel_costs = get_fuel_count_for_position(crabs)
+print("Best position: ", np.argmin(fuel_costs), "fuel use:", np.min(fuel_costs))
+
+fuel_costs = get_fuel_count_for_position(crabs, part_two=True)
+print("Best position new: ", np.argmin(fuel_costs), "fuel use:", np.min(fuel_costs))
